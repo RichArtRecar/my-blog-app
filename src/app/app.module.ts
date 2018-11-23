229d8293a9c90c36583bba691fcfc3d02583f14f
Rich Recar <richard_recar@outlook.com>
2018-11-23 17:46:51 -0500

    Sticky

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
+import { PostsComponent } from './posts/posts.component';
 @NgModule({
   declarations: [
     AppComponent,
-    NavComponent
+    NavComponent,
+    PostsComponent
   ],
   imports: [
     BrowserModule,
