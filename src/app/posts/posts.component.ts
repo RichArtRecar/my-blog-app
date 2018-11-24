5ae8865d6765622703b632453f48d297436fb83e
Rich Recar <richard_recar@outlook.com>
2018-11-24 00:33:23 -0500

    'Gnite'

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -2,7 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-posts',
-    templateUrl: './posts.component.html'
+    templateUrl: './posts.component.html',
+
+    styleUrls: ['posts.component.css']
 })
 
 export class PostsComponent {
